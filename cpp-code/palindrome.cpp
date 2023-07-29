fe2b5e4d67ae36cd75913e3e194deb79ffac1b78
Liu kuan-Tsen <75406330+jerry762@users.noreply.github.com>
2023-07-29 20:24:47 +0800

    update

--- cpp-code/palindrome.cpp
+++ cpp-code/palindrome.cpp
@@ -3,64 +3,65 @@
 
 using namespace std;
 
-bool isPalindrome(string &aSring);
-void sort_out(string &aSring);
+bool isPalindrome(string &str);
+void sort_out(string &str);
 
 int main()
 {
     string aString;
 
-    while (true)
+    while (1)
     {
         cout << "Enter a string or X to Exit('<' == backspace) :";
         cin >> aString;
         cout << endl;
+
         if (aString.at(0) == 'x')
-        {
             break;
-        }
+
         sort_out(aString);
+
         if (isPalindrome(aString))
-        {
-            cout << "The " << aString << " is a Palindrome !" << endl;
-        }
+            cout << "The \"" << aString << "\" is a Palindrome !" << endl;
         else
-        {
-            cout << "The " << aString << " is not a Palindrome !" << endl;
-        }
+            cout << "The \"" << aString << "\" is not a Palindrome !" << endl;
+
     }
 }
-void sort_out(string &aSring)
+
+void sort_out(string &str)
 {
-    int size = aSring.size(), count = 0;
+    int size = str.size(), count = 0;
 
     for (int i = size - 1; i >= 0; i--)
     {
-        if (aSring.at(i) == '<')
+        if (str.at(i) == '<')
         {
-            aSring.erase(i, 1);
+            str.erase(i, 1);
             count++;
         }
         else
         {
             if (count != 0)
             {
-                aSring.erase(i, 1);
+                str.erase(i, 1);
                 count--;
             }
         }
     }
 }
-bool isPalindrome(string &aSring)
+
+bool isPalindrome(string &str)
 {
-    int size = aSring.size();
+    int size = str.size();
 
     for (int i = 0; i < size / 2; i++)
     {
-        if (aSring.at(i) != aSring.at(size - 1 - i))
+        if (str.at(i) != str.at(size - 1 - i))
         {
             return false;
         }
     }
+    
     return true;
 }
