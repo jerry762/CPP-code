0b8b0e7d3a1172302111da59bf2d2c81cf6f1ea4
Liu kuan-Tsen <75406330+jerry762@users.noreply.github.com>
2024-10-30 19:21:27 +0800

    Add Async Feature Version

--- algo/sort/sort_time_compare.cpp
+++ algo/sort/sort_time_compare.cpp
@@ -4,120 +4,73 @@
 #include <random>
 #include <algorithm>
 #include <iomanip>
+#include <future>
+#include <mutex>
 
 #define SIZE 50'000
 
-//* time complexity: O(n^2)
-void bubble_sort(std::vector<int> &vec);
-
-//* time complexity: O(n^2)
-void selection_sort(std::vector<int> &vec);
-
-//* time complexity: O(n^2)
-void insertion_sort(std::vector<int> &vec);
-
-//* time complexity: O(nlogn)
-void merge_sort(std::vector<int> &vec, int left, int right);
-void merge(std::vector<int> &vec, int left, int mid, int right);
-
-//* time complexity: O(nlogn)
-void quick_sort(std::vector<int> &vec, int left, int right);
-int partition(std::vector<int> &vec, int left, int right, int pivot);
-
-//* time complexity: O(nlogn)
-void heap_sort(std::vector<int> &vec);
-void heapify(std::vector<int> &vec, int size, int root);
+std::mutex m;
+static int numberNo = 0;
 
 std::random_device rd;
-std::uniform_int_distribution<int> dist(1, 100);
+std::uniform_int_distribution<int> dist;
+
+#pragma region Sort algorithm
+
+//* time complexity: O(n^2)
+void BubbleSort(std::vector<int> vec);
+
+//* time complexity: O(n^2)
+void SelectionSort(std::vector<int> vec);
+
+//* time complexity: O(n^2)
+void InsertionSort(std::vector<int> vec);
+
+//* time complexity: O(nlogn)
+void MergeSort(std::vector<int> vec);
+void MergeSort_Divide(std::vector<int> &vec, int left, int right);
+void MergeSort_Conquer(std::vector<int> &vec, int left, int mid, int right);
+
+//* time complexity: O(nlogn)
+void QuickSort(std::vector<int> vec);
+void QuickSort_Divide(std::vector<int> &vec, int left, int right);
+int QuickSort_Partition(std::vector<int> &vec, int left, int right, int pivot);
+
+//* time complexity: O(nlogn)
+void HeapSort(std::vector<int> vec);
+void HeapSort_Heapify(std::vector<int> &vec, int size, int root);
+#pragma endregion
+
+void PrintTimeAndOrder(std::string algo, std::chrono::duration<double> time_span);
 
 int main()
 {
     std::cout << "Running..." << std::endl;
 
     unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+
     std::default_random_engine gen(seed);
     std::uniform_int_distribution<int> dist(1, 1000000);
 
-    std::chrono::time_point<std::chrono::steady_clock, std::chrono::duration<double>> start, end;
-    std::chrono::duration<double> time_span_bubble, time_span_insert, time_span_select, time_span_merge, time_span_quick, time_span_heap;
-    std::vector<int> vec_ori(SIZE);
+    std::vector<int> vec(SIZE);
 
-    for (int &i : vec_ori)
+    for (int &i : vec)
         i = dist(gen);
 
-    std::vector<int> vec_bubble(vec_ori), vec_select(vec_ori), vec_insert(vec_ori), vec_merge(vec_ori), vec_quick(vec_ori), vec_heap(vec_ori);
-
-    //* bubble test
-
-    start = std::chrono::steady_clock::now();
-
-    bubble_sort(vec_bubble);
-
-    end = std::chrono::steady_clock::now();
-
-    time_span_bubble = end - start;
-
-    //* select test
-
-    start = std::chrono::steady_clock::now();
-
-    selection_sort(vec_select);
-
-    end = std::chrono::steady_clock::now();
-
-    time_span_select = end - start;
-
-    //* insert test
-
-    start = std::chrono::steady_clock::now();
-
-    insertion_sort(vec_insert);
-
-    end = std::chrono::steady_clock::now();
-
-    time_span_insert = end - start;
-
-    //* merge test
-
-    start = std::chrono::steady_clock::now();
-
-    merge_sort(vec_merge, 0, vec_merge.size() - 1);
-
-    end = std::chrono::steady_clock::now();
-
-    time_span_merge = end - start;
-
-    //* quick test
-
-    start = std::chrono::steady_clock::now();
-
-    quick_sort(vec_quick, 0, vec_quick.size() - 1);
-
-    end = std::chrono::steady_clock::now();
-
-    time_span_quick = end - start;
-
-    //* heap test
-
-    start = std::chrono::steady_clock::now();
-
-    heap_sort(vec_heap);
-
-    end = std::chrono::steady_clock::now();
-
-    time_span_heap = end - start;
-
-    std::cout << std::left << std::setw(15) << "Bubble_Sort: " << std::setw(10) << time_span_bubble.count() << "s" << std::endl;
-    std::cout << std::left << std::setw(15) << "Select_Sort: " << std::setw(10) << time_span_select.count() << "s" << std::endl;
-    std::cout << std::left << std::setw(15) << "Insert_Sort: " << std::setw(10) << time_span_insert.count() << "s" << std::endl;
-    std::cout << std::left << std::setw(15) << "Merge_Sort: " << std::setw(10) << time_span_merge.count() << "s" << std::endl;
-    std::cout << std::left << std::setw(15) << "Quick_Sort: " << std::setw(10) << time_span_quick.count() << "s" << std::endl;
-    std::cout << std::left << std::setw(15) << "Heap_Sort: " << std::setw(10) << time_span_heap.count() << "s" << std::endl;
+    std::future<void> bubble = std::async(std::launch::async, BubbleSort, vec);
+    std::future<void> select = std::async(std::launch::async, SelectionSort, vec);
+    std::future<void> insert = std::async(std::launch::async, InsertionSort, vec);
+    std::future<void> merge = std::async(std::launch::async, MergeSort, vec);
+    std::future<void> quick = std::async(std::launch::async, QuickSort, vec);
+    std::future<void> heap = std::async(std::launch::async, HeapSort, vec);
 }
 
-void bubble_sort(std::vector<int> &vec)
+void BubbleSort(std::vector<int> vec)
 {
+    std::chrono::time_point<std::chrono::steady_clock, std::chrono::duration<double>> start, end;
+    std::chrono::duration<double> time_span;
+
+    start = std::chrono::steady_clock::now();
     for (size_t i = 0; i < vec.size() - 1; i++)
     {
         bool change = false;
@@ -130,14 +83,21 @@ void bubble_sort(std::vector<int> &vec)
                 change = true;
             }
         }
-
         if (!change)
             break;
     }
+    end = std::chrono::steady_clock::now();
+
+    time_span = end - start;
+    PrintTimeAndOrder("Bubble_Sort: ", time_span);
 }
 
-void selection_sort(std::vector<int> &vec)
+void SelectionSort(std::vector<int> vec)
 {
+    std::chrono::time_point<std::chrono::steady_clock, std::chrono::duration<double>> start, end;
+    std::chrono::duration<double> time_span;
+
+    start = std::chrono::steady_clock::now();
     for (size_t i = 0; i < vec.size() - 1; i++)
     {
         size_t index = i;
@@ -149,10 +109,18 @@ void selection_sort(std::vector<int> &vec)
         }
         std::swap(vec.at(i), vec.at(index));
     }
+    end = std::chrono::steady_clock::now();
+
+    time_span = end - start;
+    PrintTimeAndOrder("Select_Sort: ", time_span);
 }
 
-void insertion_sort(std::vector<int> &vec)
+void InsertionSort(std::vector<int> vec)
 {
+    std::chrono::time_point<std::chrono::steady_clock, std::chrono::duration<double>> start, end;
+    std::chrono::duration<double> time_span;
+
+    start = std::chrono::steady_clock::now();
     for (size_t i = 1; i < vec.size(); i++)
     {
         int key = vec.at(i);
@@ -165,21 +133,38 @@ void insertion_sort(std::vector<int> &vec)
         }
         vec.at(j + 1) = key;
     }
+    end = std::chrono::steady_clock::now();
+
+    time_span = end - start;
+    PrintTimeAndOrder("Insert_Sort: ", time_span);
 }
 
-void merge_sort(std::vector<int> &vec, int left, int right)
+void MergeSort(std::vector<int> vec)
+{
+    std::chrono::time_point<std::chrono::steady_clock, std::chrono::duration<double>> start, end;
+    std::chrono::duration<double> time_span;
+
+    start = std::chrono::steady_clock::now();
+    MergeSort_Divide(vec, 0, vec.size() - 1);
+    end = std::chrono::steady_clock::now();
+
+    time_span = end - start;
+    PrintTimeAndOrder("Merge_Sort: ", time_span);
+}
+
+void MergeSort_Divide(std::vector<int> &vec, int left, int right)
 {
     if (left >= right)
         return;
 
     int mid = (left + right) / 2;
 
-    merge_sort(vec, left, mid);      //* divide
-    merge_sort(vec, mid + 1, right); //* divide
-    merge(vec, left, mid, right);    //* combine
+    MergeSort_Divide(vec, left, mid);
+    MergeSort_Divide(vec, mid + 1, right);
+    MergeSort_Conquer(vec, left, mid, right);
 }
 
-void merge(std::vector<int> &vec, int left, int mid, int right)
+void MergeSort_Conquer(std::vector<int> &vec, int left, int mid, int right)
 {
     std::vector<int> leftSubArr(vec.begin() + left, vec.begin() + mid + 1);
     std::vector<int> rightSubArr(vec.begin() + mid + 1, vec.begin() + right + 1);
@@ -205,20 +190,33 @@ void merge(std::vector<int> &vec, int left, int mid, int right)
     }
 }
 
-void quick_sort(std::vector<int> &vec, int left, int right)
+void QuickSort(std::vector<int> vec)
+{
+    std::chrono::time_point<std::chrono::steady_clock, std::chrono::duration<double>> start, end;
+    std::chrono::duration<double> time_span;
+
+    start = std::chrono::steady_clock::now();
+    QuickSort_Divide(vec, 0, vec.size() - 1);
+    end = std::chrono::steady_clock::now();
+
+    time_span = end - start;
+    PrintTimeAndOrder("Quick_Sort: ", time_span);
+}
+
+void QuickSort_Divide(std::vector<int> &vec, int left, int right)
 {
     if (left >= right)
         return;
 
     dist.param(decltype(dist)::param_type(left, right));
 
-    int pivot = partition(vec, left, right, dist(rd));
+    int pivot = QuickSort_Partition(vec, left, right, dist(rd));
 
-    quick_sort(vec, left, pivot - 1);
-    quick_sort(vec, pivot + 1, right);
+    QuickSort_Divide(vec, left, pivot - 1);
+    QuickSort_Divide(vec, pivot + 1, right);
 }
 
-int partition(std::vector<int> &vec, int left, int right, int pivot)
+int QuickSort_Partition(std::vector<int> &vec, int left, int right, int pivot)
 {
     int value = vec.at(pivot);
     int storeIndex = left;
@@ -239,26 +237,30 @@ int partition(std::vector<int> &vec, int left, int right, int pivot)
     return storeIndex;
 }
 
-void heap_sort(std::vector<int> &vec)
+void HeapSort(std::vector<int> vec)
 {
-    //* build heap
-    for (int i = vec.size() / 2 - 1; i >= 0; i--)
-        heapify(vec, vec.size(), i);
+    std::chrono::time_point<std::chrono::steady_clock, std::chrono::duration<double>> start, end;
+    std::chrono::duration<double> time_span;
+
+    start = std::chrono::steady_clock::now();
+    for (int i = vec.size() / 2 - 1; i >= 0; i--) //* build heap
+        HeapSort_Heapify(vec, vec.size(), i);
 
     for (int i = vec.size() - 1; i > 0; i--)
     {
         std::swap(vec.at(0), vec.at(i));
-
-        heapify(vec, i, 0);
+        HeapSort_Heapify(vec, i, 0);
     }
+    end = std::chrono::steady_clock::now();
+
+    time_span = end - start;
+    PrintTimeAndOrder("Heap_Sort: ", time_span);
 }
 
-void heapify(std::vector<int> &vec, int size, int root)
+void HeapSort_Heapify(std::vector<int> &vec, int size, int root)
 {
     int max = root;
-
     int l = 2 * root + 1;
-
     int r = 2 * root + 2;
 
     if (l < size && vec.at(l) > vec.at(max))
@@ -270,7 +272,14 @@ void heapify(std::vector<int> &vec, int size, int root)
     if (max != root)
     {
         std::swap(vec.at(root), vec.at(max));
-
-        heapify(vec, size, max);
+        HeapSort_Heapify(vec, size, max);
     }
+}
+
+void PrintTimeAndOrder(std::string algo, std::chrono::duration<double> time_span)
+{
+    std::lock_guard<std::mutex> lk(m);
+
+    std::cout << std::left << std::setw(15) << algo
+              << std::setw(10) << time_span.count() << "s <- " << ++numberNo << std::endl;
 }
